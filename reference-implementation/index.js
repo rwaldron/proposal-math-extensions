bfe041b856318614f968be07cd8697c37a0394f0
Christoph Päper <bugzilla@crissov.de>
2020-09-15 12:47:29 +0200

    remove predefined clock and compass angles
    as requested in review

--- reference-implementation/index.js
+++ reference-implementation/index.js
@@ -23,35 +23,7 @@
   defineMath("DEG_PER_RAD", Math.PI / 180);
   defineMath("RAD_PER_DEG", 180 / Math.PI);
   defineMath("TAU", 2 * Math.PI);
-  defineMath("CLOCK1", Math.PI / 6);
-  defineMath("CLOCK2", Math.PI / 3);
-  defineMath("CLOCK3", Math.PI / 2);
-  defineMath("CLOCK4", Math.CLOCK2 * 2);
-  defineMath("CLOCK5", Math.CLOCK1 * 5);
-  defineMath("CLOCK6", Math.PI);
-  defineMath("CLOCK7", Math.CLOCK1 * 7);
-  defineMath("CLOCK8", Math.CLOCK2 * 4);
-  defineMath("CLOCK9", Math.CLOCK3 * 3);
-  defineMath("CLOCK10", Math.CLOCK2 * 5);
-  defineMath("CLOCK11", Math.CLOCK1 * 11);
-  defineMath("CLOCK12", Math.TAU);
-  defineMath("NORTH", Math.TAU);
-  defineMath("NNE", Math.PI / 8);
-  defineMath("NE", Math.PI / 4);
-  defineMath("ENE", Math.NNE * 3);
-  defineMath("EAST", Math.PI / 2);
-  defineMath("ESE", Math.NNE * 5);
-  defineMath("SE", Math.NE * 3);
-  defineMath("SSE", Math.NNE * 7);
-  defineMath("SOUTH", Math.PI);
-  defineMath("SSW", Math.NNE * 9);
-  defineMath("SW", Math.NE * 5);
-  defineMath("WSW", Math.NNE * 11);
-  defineMath("WEST", Math.EAST * 3);
-  defineMath("WNW", Math.NNE * 13);
-  defineMath("NW", Math.NE * 7);
-  defineMath("NNW", Math.NNE * 15);
-
+  
   const f32A = new Float32Array(1);
 
   defineMath("scale", function scale(x, inLow, inHigh, outLow, outHigh) {
